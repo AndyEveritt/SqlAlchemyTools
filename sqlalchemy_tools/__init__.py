cacd40eca8048a0f00ed576b6c6a03584c10907e
AndyEveritt <aajeveritt@gmail.com>
2020-12-03 21:25:17 +0000

    expose imports

--- sqlalchemy_tools/__init__.py
+++ sqlalchemy_tools/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "0.1.0-b0"
 
-from .database import Database
+from .database import Database, BaseModel, BaseQuery
+from .migration import Migrate, migrate_manager
 from .forms import create_model_form
