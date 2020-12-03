6c72b841f471b0b039b3b1e52e908b9def05629f
AndyEveritt <aajeveritt@gmail.com>
2020-12-03 23:22:56 +0000

    update migration method docs

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@ from sqlalchemy_tools import Database
 
 
 db = Database('sqlite:///tmp.db')
-migrate = Migrate(db)
+migrate = Migrate(db, render_as_batch=True)
 migrate_manager.set_migrate(migrate)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-    surname = db.Column(db.String(128))
+    # surname = db.Column(db.String(128))
 
 
 if __name__ == '__main__':
