f90d60e7a6474cd0b3d6cfcd0a98da3d37b974f8
AndyEveritt <andreweveritt@e3d-online.com>
2020-12-07 11:37:41 +0000

    0.1.0-b5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="SqlAlchemy_Tools",
-    version="0.1.0-b4",
+    version="0.1.0-b5",
     packages=find_packages(),
     include_package_data=True,
 
--- sqlalchemy_tools/__init__.py
+++ sqlalchemy_tools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0-b4"
+__version__ = "0.1.0-b5"
 
 from .database import Database, BaseModel, BaseQuery
 from .migration import Migrate, migrate_manager
